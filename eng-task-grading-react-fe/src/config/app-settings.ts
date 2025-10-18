01ca3f07e24a61872c785178aa27b58502f6a995
Marek Vajgl <engin@seznam.cz>
2025-10-19 00:07:15 +0200

    Make All Runable

--- eng-task-grading-react-fe/src/config/app-settings.ts
+++ eng-task-grading-react-fe/src/config/app-settings.ts
@@ -1,8 +1,8 @@
 const AppSettings = {
   apiUrl: import.meta.env.VITE_API_URL,
-  backendUrl: "https://localhost:55556/api",
+  backendUrl: import.meta.env.BACKEND_URL,
   cloudflare: {
-    enabled: false,
+    enabled: import.meta.env.CLOUDFLARE_ENABLED,
     siteKey: import.meta.env.VITE_CLOUDFLARE_SITE_KEY,
   },
   logLevel: "debug",
